4f7213af30c8493ca8ace67fc132e653aa8dd337
LiaGlitcheur <103276744+LiaGlitcheur@users.noreply.github.com>
2024-05-04 14:49:22 +0200

    Slyver-Launcher1.0.2

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -4,7 +4,7 @@ const ConfigManager = require('./configmanager')
 
 // Old WesterosCraft url.
 // exports.REMOTE_DISTRO_URL = 'http://192.168.1.50/Slyver/distribution.json'
-exports.REMOTE_DISTRO_URL = 'http://192.168.1.50/Slyver/distribution.json'
+exports.REMOTE_DISTRO_URL = 'http://88.169.6.106:45600/Slyver/distribution.json'
 
 const api = new DistributionAPI(
     ConfigManager.getLauncherDirectory(),
