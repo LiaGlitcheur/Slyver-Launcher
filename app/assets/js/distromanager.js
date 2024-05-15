dc9024aab0c84e9b04a44a3a103553b504b368ff
LiaGlitcheur <103276744+LiaGlitcheur@users.noreply.github.com>
2024-05-15 15:18:07 +0200

    v1.0.3

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -3,8 +3,8 @@ const { DistributionAPI } = require('helios-core/common')
 const ConfigManager = require('./configmanager')
 
 // Old WesterosCraft url.
-// exports.REMOTE_DISTRO_URL = 'http://192.168.1.50/Slyver/distribution.json'
-exports.REMOTE_DISTRO_URL = 'http://88.169.6.106:45600/Slyver/distribution.json'
+// exports.REMOTE_DISTRO_URL = 'http://192.168.1.50/distribution.json'
+exports.REMOTE_DISTRO_URL = 'http://88.169.6.106:45600/distribution.json'
 
 const api = new DistributionAPI(
     ConfigManager.getLauncherDirectory(),
