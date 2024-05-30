e332ee674d3533e80b5b544ba5be4e12d59513dc
LiaGlitcheur <103276744+LiaGlitcheur@users.noreply.github.com>
2024-05-30 18:35:45 +0200

    Changement d'URL
    changement de l'url du site, passant à slyver.fr, également un changement de l'url du serveur Minecraft, rien ne changera du côté expérience utilisateur.

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -3,8 +3,8 @@ const { DistributionAPI } = require('helios-core/common')
 const ConfigManager = require('./configmanager')
 
 // Old WesterosCraft url.
-// exports.REMOTE_DISTRO_URL = 'http://mc.westeroscraft.com/WesterosCraftLauncher/distribution.json'
-exports.REMOTE_DISTRO_URL = 'http://88.169.6.106:45600/distribution.json'
+// exports.REMOTE_DISTRO_URL = 'https://slyver.fr/distribution.json'
+exports.REMOTE_DISTRO_URL = 'https://slyver.fr/distribution.json'
 
 const api = new DistributionAPI(
     ConfigManager.getLauncherDirectory(),
